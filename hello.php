424c5e36862627092135fbbebe9a41e2c80c7100
maria sadovnikova <msadovnikov904@gmail.com>
2021-09-30 12:34:21 +0800

    Перенесла проект в другую папку

---
+++ hello.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- … -->
+</head>
+<body>
+    Я новая страничка!!!1111 =)
+    <br>
+    <img src="/img/025_picture-47918.gif" alt="Стас Барецкий упал с лестницы">
+</body>
+</html>
