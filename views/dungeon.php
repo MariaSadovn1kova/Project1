1e6c17aa098bab9d350a57f6d62dd7b7aed80554
maria sadovnikova <msadovnikov904@gmail.com>
2021-10-10 13:23:35 +0800

    Доверстала страничку лесного союза

--- views/dungeon.php
+++ views/dungeon.php
@@ -25,6 +25,8 @@
             <img src="https://lh3.googleusercontent.com/proxy/p34oN3kIW5kcMRZX0g_PK54xAehn6rPe3CjikzxvnKGcYitdK3RrNyrULcy4uR_3jM7tfy_i66udehJvujTs-xloEDmMbIGV6cMARyFkU04te8oVM2vmr7U7kIplr-maFQQzXoFP5yt3SW9k98OTFRaC4gyN" alt="">
         <?php }  else if ($is_info) { ?>
             Тёмные эльфы – это потомки обычных, лесных эльфов, они отвернулись от поклонения Силанне, теперь их будоражит тьма и секреты, находящиеся в ней. Они ренегаты, подобно некромантам, позже превратившиеся в отдельную расу. Главная жизненная цель тёмного эльфа – познать все секреты тьмы и научиться управлять ею. Эти жители подземелий поклоняются Малассе, которая изредка посвящает высших жрецов тёмных эльфов и безликих в секреты тьмы.
+        <?php }  else  { ?>
+            Лига Теней (ориг. Dungeon) – фракция, добавленная в игре Heroes of Might and Magic V.
         <?php }  ?>
     </li>
 </ul>
